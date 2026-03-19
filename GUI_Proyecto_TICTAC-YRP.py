eb9762dbe3afe4c86c42c2a8d9f5fb8701c9e9ef
paolarazo <250911@utags.edu.mx>
2026-03-19 13:09:24 -0600

    Update GUI_Proyecto_TIC-TAC YRP.py

--- GUI_Proyecto_TICTAC-YRP.py
+++ GUI_Proyecto_TICTAC-YRP.py
@@ -7,7 +7,7 @@ ventana = tk.Tk()
 ventana.title("GUI_Proyecto_TIC-TAC_YRP")
 #decalramos el tamaño de la ventana 
 ventana.geometry("1000x600")
-#elegimos colores beige por que son colores calidos que no molestan
+#elegimos colores azules por que son colores armonioso
 ventana.configure(bg="#9BB7CC")  #se declaro el fondo de la ventana 
 
 # Declaramos la paleta de colores a aplicar en la ventana 
@@ -25,18 +25,16 @@ encabezado.pack(fill="x")
 titulo = tk.Label(encabezado, text="TIC-TAC RUTA", bg=color_header, 
                   fg=color_accent, font=("Segoe UI", 22, "bold"))
 titulo.place(x=20, y=20)
-
+#Declaramos la variable encabezado para poner un subtitulo para la empresa que le correspode el codigo 
 subtitulo = tk.Label(encabezado, text="Coordinación General de Movilidad - Aguascalientes", 
                      bg=color_header, fg="#F5EFEB", font=("Segoe UI", 10, "italic"))
 subtitulo.place(x=25, y=55)
 
-# ==========================================
-# Menú lateral (Widget: Frame)
-# ==========================================
+#Declaramos el color del menu 
 menu = tk.Frame(ventana, bg=color_menu, width=200)
 menu.pack(side="left", fill="y")
 
-# Widgets: Button (Navegación principal) [cite: 107]
+# Declaramos las variables que se encontraran en el menu y que se podran presionar 
 btn_inicio = tk.Button(menu, text="Panel de Control", bg=color_menu, fg="white", 
                        relief="flat", font=("Segoe UI", 11), activebackground=color_header)
 btn_inicio.pack(fill="x", pady=20, padx=10)
@@ -45,14 +43,11 @@ btn_ayuda = tk.Button(menu, text="Soporte Técnico", bg=color_menu, fg="white",
                       relief="flat", font=("Segoe UI", 11))
 btn_ayuda.pack(fill="x", pady=10, padx=10)
 
-# ==========================================
-# Área de Contenido (Widget: Frame) 
-# ==========================================
+#Declaramos el tamaño del area de contenido de la ventana 
 contenido = tk.Frame(ventana, bg=color_content)
 contenido.pack(side="left", fill="both", expand=True, padx=30, pady=30)
 
-# Widget: LabelFrame (Bloque de Validación) [cite: 105]
-# Se eliminó el borde visual (borderwidth=0) y el título del LabelFrame para que parezca un solo bloque
+#se pone como etiqueta la verificacion del camion (Esto esta en duda ya que pensandolo en equipo presenta incosistencia en nuestra logia , lo checaremos)
 lf_acceso = tk.LabelFrame(contenido, text="", bg=color_content, 
                           font=("Segoe UI", 12, "bold"), fg=color_accent, borderwidth=0)
 lf_acceso.pack(fill="x", padx=10, pady=10)
@@ -60,29 +55,28 @@ lf_acceso.pack(fill="x", padx=10, pady=10)
 lbl_tarjeta = tk.Label(lf_acceso, text="ID Tarjeta de Ruta:", bg=color_content, font=("Segoe UI", 11), fg=color_accent)
 lbl_tarjeta.grid(row=0, column=0, padx=20, pady=30)
 
-# Widget: Entry (Caja de entrada) [cite: 65, 67]
+# Se declara la caja de texto para que ingresen el ID 
 entrada_id = tk.Entry(lf_acceso, width=25, font=("Segoe UI", 11))
 entrada_id.grid(row=0, column=1, padx=10)
 
-# Widget: Button [cite: 112]
+# Se declara el boton para validar que la informacion es correcta (del ID)
 btn_entrar = tk.Button(lf_acceso, text="Validar", bg=color_header, fg="white", font=("Segoe UI", 10, "bold"))
 btn_entrar.grid(row=0, column=2, padx=20)
 
-# Widget: LabelFrame (Panel de Monitoreo) [cite: 55]
-# Se eliminó el borde visual (borderwidth=0) y el título del LabelFrame para unificar la vista
+
 lf_monitoreo = tk.LabelFrame(contenido, text="", bg=color_content, 
                              font=("Segoe UI", 12, "bold"), fg=color_accent, borderwidth=0)
 lf_monitoreo.pack(fill="both", expand=True, padx=10, pady=10)
 
-# Widget: Combobox [cite: 80, 117]
+# Se pone un boton selectivo que hara el trabajo de que el usuario pueda ingresar que ruta quiere saber su posicion 
 lbl_sel = tk.Label(lf_monitoreo, text="Seleccionar Ruta:", bg=color_content, font=("Segoe UI", 11), fg=color_accent)
 lbl_sel.place(x=30, y=30)
-
-combo = ttk.Combobox(lf_monitoreo, values=["Ruta 50 - Directo", "Ruta 20", "Ruta 40"], state="readonly")
+#Nos faltan rutas por poner pero por el ejemplo implementamos las rutas que salen de la UTA
+combo = ttk.Combobox(lf_monitoreo, values=["Ruta 50 ", "Ruta 50-B", "Ruta 41-penal","Ruta 25"], state="readonly")
 combo.current(0)
 combo.place(x=160, y=32)
 
-# Widgets: Label (Estructura de Información sin valores fijos) [cite: 58, 108]
+# Estructuramos los valores predominados que seran puestos por funciones , el usuario solo tiene la capacidad de verlos no de modificar esto 
 lbl_camion = tk.Label(lf_monitoreo, text="UBICACIÓN ACTUAL DEL CAMIÓN:", 
                       bg="#C8DDE6", fg="#1B5A5E", font=("Segoe UI", 12, "bold"), width=60, pady=10)
 lbl_camion.place(x=30, y=100)
@@ -94,6 +88,5 @@ lbl_paradas.place(x=30, y=160)
 lbl_tiempo = tk.Label(lf_monitoreo, text="TIEMPO DE APROXIMACIÓN SIGUIENTE PARADA:", 
                       bg="#BCDBFF", fg="#0CA4BF", font=("Segoe UI", 12, "bold"), width=60, pady=10)
 lbl_tiempo.place(x=30, y=220)
-
-# ==========================================
+#Cerramos la ventana 
 ventana.mainloop()
